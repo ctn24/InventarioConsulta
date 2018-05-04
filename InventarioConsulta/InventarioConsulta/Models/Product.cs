cb7c9c8535f2016025cca024b7e6e34d2168ddff
ctn24ctn <ctn24ctn@users.noreply.github.com>
2018-05-04 10:41:01 -0500

    Models

---
+++ InventarioConsulta/InventarioConsulta/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿namespace InventarioConsulta.Models
+{
+
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+
+    public class Product
+    {
+        public string Name { get; set; }
+
+        public List<string> TopLevelDomain { get; set; }
+
+        public string Alpha2Code { get; set; }
+
+        public string Alpha3Code { get; set; }
+
+        public List<string> CallingCodes { get; set; }
+
+        public string Capital { get; set; }
+
+        public List<string> AltSpellings { get; set; }
+
+        public string Region { get; set; }
+
+        public List<double> Latlng { get; set; }
+
+    }
+}
+}
